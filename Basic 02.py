c3d00a5be6e52407206b28910d7c5197b543329a
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-03 15:15:28 +0600

    2nd problem solve

---
+++ Basic 02.py
@@ -0,0 +1,21 @@
+"""
+Task ----:
+Write a Python program to find out what version of Python you are using.
+
+"""
+# First way this problem solve!
+import sys
+
+print("python version")
+print(sys.version)
+print("python version_info")
+print(sys.version_info)
+
+# Second way this problem solve!
+import platform
+
+print("python version", platform.python_version())
+
+# thread way this problem solve!
+# press commend for python terminal
+# just type --- (python --version) for result
