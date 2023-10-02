112d39a52bb90c2dad0f21b57f23a3b0922afc9f
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2023-10-02 23:47:05 +0600

    find the largest among the two numbers

---
+++ Basic 09.py
@@ -0,0 +1,23 @@
+"""
+Conditions  >>> Find the Greatest of the Two Numbers
+
+Given two integer inputs as number1 and number2, the objective is to find the largest among the two numbers.
+Therefore, we’ll write a code to Find the Greatest of the Two Numbers in Python Language.
+
+Input       >>> 20 40
+
+Output      >>> 40
+"""
+# write code
+number1 , number2 = int(input()), int(input())
+if number1 > number2 :
+    print(number1)
+else:
+    print(number2)
+
+# write code but wrong output ! input tru to >>>>>>> 100 99
+#
+# total = number1 , number2 = input().split()
+#
+# print(max(total))
+
