a809490a7863dcff2eea1b692ff67f6645f3907c
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2023-10-02 23:01:39 +0600

    Find the Sum of Numbers in a given Range in Python

---
+++ Basic 08.py
@@ -0,0 +1,16 @@
+"""
+Conditions  >>> Find the Sum of Numbers in a given Range in Python ?
+
+Input       >>> 2 5
+
+Output      >>> 14
+"""
+
+num = 0
+num1 , num2 = input(
+    "Enter the two int type value : ").split()  # input data different value simple use to split()--function
+print("input data dev len")
+for i in range(int(num1) , int(num2) + 1):
+    num += i
+    print(i)  # data len print out to i . not a num variable ?
+print("sum of number: " , num)
