90150fa72db5402acace405a46d586b9a2b7fc49
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2023-10-06 17:01:38 +0600

    Reverse of Input Number in python

---
+++ Basic 14.py
@@ -0,0 +1,13 @@
+"""
+Conditions  >>> Find the Reverse of a Number in Python
+
+Given an integer input the objective is to reverse the given integer number using loops and slicing. Therefore,
+we’ll write a program to Find the Reverse of a Number in Python Language.
+
+Input       >>> 12345
+
+Output      >>> 54321
+"""
+
+num = input("Enter The Value :")
+print(num[::-1])  # just flow this simple task
